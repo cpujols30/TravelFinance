8ef171ab6f33455bcb5c68e9d41702adc7e65ab1
cpujols30 <9004187@alumnos.ufv.es>
2024-05-01 16:03:53 +0200

    PRUEBA DE SERVICIO CON BACKEND

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 import toast from 'react-hot-toast';
 
-const BASE_URL = 'http://localhost:8080'
-
+//const BASE_URL = 'http://localhost:8080'
+const BASE_URL = 'https://travelfinancebackend.azurewebsites.net';
 export const login = async credentials => {
   const { username, password } = credentials
   const token = btoa(`${username}:${password}`) // Codifica las credenciales en Base64
